ac33837eb27399f124b0a858de382a658b13b905
Lucy Guo <lucyguo8@gmail.com>
2018-07-19 12:02:38 -0700

    Add new data

--- app.py
+++ app.py
@@ -1,68 +1,62 @@
-#change variable names
-
 import mpl_toolkits
 from mpl_toolkits.mplot3d import Axes3D
-
 from collections import OrderedDict
 from  matplotlib import pyplot as plt
 import numpy as np
 import csv
 
-
-# READ DATA AND Z
-
 # ask user for input files of data and index
-inputFile = 'data.txt'
-#raw_input("Enter the name of your matrix file: ") #ex: data.txt
-inputFileNum = 'data2.txt'
-#raw_input("Enter the name of vector file: ") #ex: data2.txt
+inputMatrix = raw_input("Enter the name of your matrix file: ") #ex: data.txt
+inputVector = raw_input("Enter the name of vector file: ") #ex: data2.txt
 
 # create dictionary to add lists of values to
 indexValues = {}
 #create lists to add data to
-data_input =[] #the matrix
-data_input2 = [] #the vector
+matrix =[] #the matrix
+vector = [] #the vector
 
 #read the input file data
-with open (inputFile, 'rb') as csvfile:
+with open (inputMatrix, 'rb') as csvfile:
     inputData = csv.reader(csvfile)
     for row in inputData:
-        data_input.append(row)
+        matrix.append(row)
 
 #read the input file data2
-with open (inputFileNum, 'rb') as csvfile:
+with open (inputVector, 'rb') as csvfile:
     inputData2 = csv.reader(csvfile)
     for row in inputData2:
-            data_input2.append(row)
+            vector.append(row)
 
 #remove whitespaces in data
-for item in data_input:
+for item in matrix:
     for i in range(len(item)):
         item[i] = item[i].replace(" ","")
 
 #remove whitespaces in data2
-for item in data_input2:
+for item in vector:
     for i in range(len(item)):
         item[i] = item[i].replace(" ","")
 
-n = len(data_input[0])
+n = len(matrix[0])
 
-data_input  = np.array(data_input, dtype=np.float_)
-data_input2 = np.array(data_input2, dtype=np.float_).flatten()
+#convert from string to float
+matrix  = np.array(matrix, dtype=np.float_)
+vector = np.array(vector, dtype=np.float_).flatten()
 
 fig, axs = plt.subplots(n, n, subplot_kw={'projection': '3d'})
 
+#create graphs
 for rowid in range(n):
     for colid in range(n):
         if (rowid == colid):
             #iterate through list of given data to add to dictionary indexValues
-            for i in range(len(data_input)):
-                if data_input[i][rowid] in indexValues:
+            for i in range(len(matrix)):
+                if matrix[i][rowid] in indexValues:
                     # append the corresponding value over in the other matrix
-                    indexValues[data_input[i][rowid]].append(data_input2[i])
+                    indexValues[matrix[i][rowid]].append(vector[i])
                 else:
                     #add the value to the list with identifier j[d] in the dictionary
-                    indexValues[data_input[i][rowid]] = [data_input2[i]]
+                    indexValues[matrix[i][rowid]] = [vector[i]]
 
             # create ordered dictionary to sort indexValues with
             sorted_indexValues = OrderedDict()
@@ -97,12 +91,8 @@ for rowid in range(n):
             axs[rowid][colid].set_zticklabels([])
             axs[rowid][colid].view_init(-90, -90)
 
-
-            # axs[rowid][colid].set_xticklabels(rotation=30)
-            
-
         elif (rowid < colid):
-            axs[rowid][colid].plot_trisurf(data_input[:,rowid], data_input[:,colid], data_input2, linewidth=0.2, antialiased=True, )
+            axs[rowid][colid].plot_trisurf(matrix[:,rowid], matrix[:,colid], vector, linewidth=0.2, antialiased=True, )
             axs[rowid][colid].set_xlabel("Col " + str(rowid))
             axs[rowid][colid].set_ylabel("Col " + str(colid))
             axs[rowid][colid].set_zlabel("Vector")
@@ -112,7 +102,6 @@ for rowid in range(n):
 
 plt.suptitle("Multiverse")
 plt.show()
-
 
 # , 7
 # , 0
@@ -133,4 +122,3 @@ plt.show()
 # , 3
 # , 2
 # , 6
-
