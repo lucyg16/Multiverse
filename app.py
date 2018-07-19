368e154e3d7d20aa41a8c9aa9a53f454af2588d0
Lucy Guo <lucyguo8@gmail.com>
2018-07-19 11:53:06 -0700

    Rotate box plots

--- app.py
+++ app.py
@@ -94,12 +94,20 @@ for rowid in range(n):
 
             axs[rowid][colid].set_xlabel("Col " + str(rowid))
             axs[rowid][colid].set_ylabel("Value")
+            axs[rowid][colid].set_zticklabels([])
+            axs[rowid][colid].view_init(-90, -90)
+
+
+            # axs[rowid][colid].set_xticklabels(rotation=30)
+            
 
         elif (rowid < colid):
             axs[rowid][colid].plot_trisurf(data_input[:,rowid], data_input[:,colid], data_input2, linewidth=0.2, antialiased=True, )
             axs[rowid][colid].set_xlabel("Col " + str(rowid))
             axs[rowid][colid].set_ylabel("Col " + str(colid))
             axs[rowid][colid].set_zlabel("Vector")
+
+
 
 plt.suptitle("Multiverse")
 plt.show()
